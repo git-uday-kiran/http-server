debe51cbd98943d29d1bcf3c5be1f833b432efd8
git-uday-kiran <udaykiran0486@gmail.com>
2024-09-02 16:54:45 +0530

    listening on 4221 and handling http requests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.Map;
 
 public class Main {
 	public static void main(String[] args) {
@@ -19,7 +18,6 @@ public class Main {
 //				.body(Map.of("Hello", "World"))
 				.lineBreak()
 				.getResponse();
-			System.out.println(response);
 			outputStream.write(response.getBytes());
 			outputStream.flush();
 			socket.close();
